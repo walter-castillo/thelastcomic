af3b82cd1c84fe2febe2c356d9821b64fe0c6e09
David <boullhesendavid@gmail.com>
2022-05-26 21:19:32 -0300

    completo dropdown

--- navbar.js
+++ navbar.js
@@ -1,3 +1,5 @@
 const btnMenu = document.querySelector("#btnMenu");
 const menuList = document.querySelector("#menuList");
 btnMenu.addEventListener('click', () => menuList.classList.toggle("hidden-menu"));
+
+document.querySelector(".dropdown").addEventListener("click",function(){document.querySelector(".dropdown ul").classList.toggle("show");})
